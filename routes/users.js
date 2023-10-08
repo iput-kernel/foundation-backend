544c1f76a96af79ae0ac933f184046950280e8e2
bitcat <kurohaj96@gmail.com>
2023-10-08 16:37:31 +0900

    クラスにクラスメートとして参加する機能を追加

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,11 @@
 const httpStatus = require("http-status");
 
 const router = require("express").Router();
+
 const User = require("../models/User");
+const Class = require('../models/Class');
+
+const { authenticateJWT } = require("../jwtAuth");
 
 router.get("/:id", async (req, res) => {
   try {
@@ -107,6 +111,45 @@ router.put("/:id/unfollow", async (req, res) => {
       .status(httpStatus.FORBIDDEN)
       .json("自分をフォロー解除できません");
   }
+});
+
+router.put('/joinClass', authenticateJWT, async (req, res) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+        const currentUserId = req.user.id; // トークンから取得したユーザーID
+        const { classId } = req.body; // リクエストボディからclassIdを取得
+
+        const currentUser = await User.findById(currentUserId).session(session);
+        if (!currentUser) {
+            throw new Error('User not found');
+        }
+
+        const targetClass = await Class.findById(classId).session(session);
+        if (!targetClass) {
+            throw new Error('Class not found');
+        }
+
+        // ユーザーをクラスのstudentsIdに追加
+        if (!targetClass.studentsId.includes(currentUserId)) {
+            targetClass.studentsId.push(currentUserId);
+            await targetClass.save({ session });
+        }
+
+        // ユーザーのclassフィールドにクラスIDを設定
+        currentUser.class = classId;
+        await currentUser.save({ session });
+
+        await session.commitTransaction();
+        session.endSession();
+
+        res.status(200).json({ message: 'Classmate added successfully' });
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+        res.status(500).json({ message: error.message });
+    }
 });
 
 module.exports = router;
