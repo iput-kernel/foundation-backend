9b061fb38af04ef0e09568cb6134e1e1d58bf5d7
bitcat <kurohaj96@gmail.com>
2023-12-23 01:10:53 +0900

    iroiro

--- models/Timetable.ts
+++ models/Timetable.ts
@@ -6,28 +6,49 @@ export type TimetableType = {
   weekRooms: mongoose.Schema.Types.ObjectId[][];
 };
 
+const TimetableEntrySchema = new mongoose.Schema({
+  subject: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Subject",
+  },
+  room: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Room",
+  },
+});
+
 const TimetableSchema = new mongoose.Schema<TimetableType>({
   usedClass: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Class",
   },
-  weekSubjects: [
-    [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Subject",
-      },
+  weekSubjects: {
+    type: [
+      [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Subject",
+        },
+      ],
     ],
-  ],
-  weekRooms: [
-    [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Room",
-      },
+    validate: [arrayLimit, '{PATH} exceeds the limit of array length']
+  },
+  weekRooms: {
+    type: [
+      [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Room",
+        },
+      ],
     ],
-  ],
+    validate: [arrayLimit, '{PATH} exceeds the limit of array length']
+  },
 });
+
+function arrayLimit(val:[][]) {
+  return val.length <= 5 && val.every(innerArray => innerArray.length <= 7);
+}
 
 const TimeTable = mongoose.model<TimetableType>("Timetable", TimetableSchema);
 export default TimeTable;
