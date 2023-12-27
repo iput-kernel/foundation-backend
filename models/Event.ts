8ea18227f7b66828685709d2c0042e75b4cd596a
bitcat <kurohaj96@gmail.com>
2023-12-28 00:31:54 +0900

    型付け

--- models/Event.ts
+++ models/Event.ts
@@ -1,11 +1,21 @@
-import mongoose from 'mongoose';
+import mongoose, { Document } from 'mongoose';
+
+interface IEvent extends Document {
+    name: string;
+    startDate: Date;
+    endDate: Date;
+    authenticity: number;
+    feedback: mongoose.Types.ObjectId[];
+    description: string;
+}
 
 const EventSchema = new mongoose.Schema({
     name: String,
     startDate: Date,
     endDate: Date,
     authenticity: Number,
+    feedback: [mongoose.Types.ObjectId],
     description: String,
 });
 
-export default mongoose.model('Event', EventSchema);
+export default mongoose.model<IEvent>('Event', EventSchema);
---
+++ routes/event.ts
@@ -0,0 +1,10 @@
+import { Request, Response, Router as expressRouter } from "express";
+import httpStatus from "http-status";
+import Event from "../models/Event";
+
+const router = expressRouter();
+
+
+
+
+module.exports = router;
