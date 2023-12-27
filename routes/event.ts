c05192c1ab75c6cd23b1d477590d841fcc7bc174
bitcat <kurohaj96@gmail.com>
2023-12-28 02:09:14 +0900

    最大値と最小値を超えないように修正

--- routes/event.ts
+++ routes/event.ts
@@ -17,10 +17,10 @@ router.put('/trust/:id', authenticateJWT, async (req: Request, res: Response) =>
         event.trust.push(new Types.ObjectId(req.user.id));
         switch (true) {
             case req.user.credLevel <= 2:
-                event.authenticity += 10;
+                event.authenticity = Math.min(event.authenticity + 10, 100);
                 break;
             case req.user.credLevel == 3:
-                event.authenticity += 30;
+                event.authenticity = Math.min(event.authenticity + 30, 100);
                 break;
             default:
                 event.authenticity = 100;
@@ -41,16 +41,32 @@ router.put('/distrust/:id', authenticateJWT, async (req: Request, res: Response)
         if (!event) return res.status(httpStatus.NOT_FOUND).send('Event not found');
 
         event.distrust.push(new Types.ObjectId(req.user.id));
-        switch (true) {
-            case req.user.credLevel <= 2:
-                event.authenticity -= 10;
-                break;
-            case req.user.credLevel == 3:
-                event.authenticity -= 30;
-                break;
-            default:
-                event.authenticity = 0;
-        }
+        router.put('/distrust/:id', authenticateJWT, async (req: Request, res: Response) => {
+            try {
+                if (!req.user) return res.status(httpStatus.UNAUTHORIZED).send('アカウントが認証されていません。');
+        
+                const event = await Event.findById(req.params.id);
+                if (!event) return res.status(httpStatus.NOT_FOUND).send('Event not found');
+        
+                event.distrust.push(new Types.ObjectId(req.user.id));
+                switch (true) {
+                    case req.user.credLevel <= 2:
+                        event.authenticity = Math.max(event.authenticity - 10, 0);
+                        break;
+                    case req.user.credLevel == 3:
+                        event.authenticity = Math.max(event.authenticity - 30, 0);
+                        break;
+                    default:
+                        event.authenticity = 0;
+                }
+                await event.save();
+        
+                res.send('Distrust added successfully');
+            } catch (err) {
+                res.status(httpStatus.INTERNAL_SERVER_ERROR).send('Server error');
+            }
+        });
+        await event.save();
 
         res.send('Distrust added successfully');
     } catch (err) {
