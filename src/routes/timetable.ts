60446652cb15e60b363e061c171ef289e046f114
bitcat <kurohaj96@gmail.com>
2024-01-29 07:36:33 +0900

    エラーハンドリングを修正

--- src/routes/timetable.ts
+++ src/routes/timetable.ts
@@ -91,8 +91,13 @@ timetableRoute.get('/:id', async (req, res) => {
         .json({ message: '指定されたIDの時間割は存在しません' });
     }
     res.status(httpStatus.OK).json(timetable);
-  } catch (err: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: err.message });
+  } catch (err: unknown) { 
+    if (err instanceof Error) {
+      res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: err.message });
+    } else {
+      res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: '未知のエラーが発生しました' });
+      console.log(err)
+    }
   }
 });
 
